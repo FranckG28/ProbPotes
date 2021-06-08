0cbfd1d2d5d2a22d0ff561fc0b516d78debeb850
Hugo SCHELLINGER <hschellinger@hotmail.fr>
2021-06-08 13:48:25 +0200

    chepa

---
+++ tests/ReportManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using ProbPotes.managers;
+using ProbPotes.models;
+
+namespace ProbPotes.tests
+{
+    public partial class ReportManager : Form
+    {
+        public ReportManager()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportManager_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Test(EventClass evt)
+        {
+            DatabaseManager.db.Open();
+            DataSet ds = new DataSet();
+
+            DataTable dtBilan = new DataTable();
+
+            dtBilan.Columns.Add("codeParticipant", typeof(Int32));
+            dtBilan.Columns.Add("Personne", typeof(string));
+            dtBilan.Columns.Add("Plus", typeof(double));
+            dtBilan.Columns.Add("Moins", typeof(double));
+            dtBilan.Columns.Add("Solde", typeof(double));
+
+            Dictionary<int, int> partShare = new Dictionary<int, int>();
+
+            for (int i = 0; i < evt.Guests.Count; i++)
+            {
+                OleDbCommand cdPartShare = new OleDbCommand("SELECT codeParticipant,nbParts FROM Participants WHERE codeParticipant=" + i, DatabaseManager.db);
+
+                OleDbDataReader dr = cdPartShare.ExecuteReader();
+                dr.Read();
+
+                partShare.Add(Convert.ToInt32(dr[0].ToString()), Convert.ToInt32(dr[1].ToString()));
+            }
+
+
+            foreach (KeyValuePair<int, int> val in partShare)
+            {
+                //PROCEDURE STOCKEE JSP COMMENT FAIRE ;(
+            }
+
+        }
+    }
+}
