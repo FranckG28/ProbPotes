3c2b07cac8de89c041ff0171f2eadb02db8b8c66
Hugo SCHELLINGER <hschellinger@hotmail.fr>
2021-06-09 21:48:03 +0200

    ajout de SMTPEmail dans Test pour envoyer des email

---
+++ tests/SMTPEmail.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbPotes.tests
+{
+    class SMTPEmail
+    {
+        public Boolean sendMail()
+        {
+            string from = "probpote"; //https://mail.ovh.net/roundcube/?_task=login pour accéder au mail
+            string mdp = "changethis";
+            string title = "Mail Test";//OBJET DU MESSAGE
+            string message = "message zebi";//MESSAGE
+            string to = "hugo.schellinger@gmail.com";//DESTINATAIRE
+
+            int Port = 465;
+            string SMTP = "ssl0.ovh.net";
+
+            try
+            {
+                MailMessage msg;
+
+                SmtpClient client = new SmtpClient();
+
+
+                //PARAMETRE DU MAIL
+                msg = new MailMessage(from, to, title, message);
+                client.Port = Port;
+                client.Host = SMTP;
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Credentials = new NetworkCredential(from, mdp);
+                msg.Priority = MailPriority.High;
+                msg.BodyEncoding = Encoding.UTF8;
+                msg.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+
+                //ENVOIE DU MAIL
+                client.Send(msg);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
