83def9cb84f0f0353ab00b1f8b21a934f238107a
Hugo SCHELLINGER <hschellinger@hotmail.fr>
2021-06-12 00:45:31 +0200

    generatePDF fini :)

--- services/generatePDF.cs
+++ services/generatePDF.cs
@@ -18,7 +18,11 @@ namespace ProbPotes.services
         {
             try
             {
-                string outFile = Environment.CurrentDirectory + @"\\..\\..\\BilanPDF\\BILAN_" + evt.Title + "_" + part.Name + ".pdf";
+                string path = Path.Combine(Environment.CurrentDirectory + @"\\..\\..\\BilanPDF\\"+evt.Title);
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                string outFile = path+ @"\\BILAN_"+ part.Name + ".pdf";
                 //CREATION DU DOCUMENT
                 Document doc = new Document(PageSize.A4,25f,25f,1f,25f);
                 PdfWriter.GetInstance(doc, new FileStream(outFile, FileMode.Create));
@@ -88,8 +92,6 @@ namespace ProbPotes.services
                 //ROW DU TAB
                 List<string[]> DepensePaye = new List<string[]>();
                 List<string[]> DepenseBeneficie = new List<string[]>();
-                EventManager em = new EventManager();
-                WOWTW wwPart = em.GetWOWTWsPart(evt, part);
 
                 foreach (Expense expense in evt.Expenses.Expenses)
                 {
@@ -97,7 +99,7 @@ namespace ProbPotes.services
                     {
                         string[] s = new string[2];
                         s[0] = expense.description;
-                        s[1] = expense.sum.ToString();
+                        s[1] = expense.sum.ToString()+" €";
                         DepensePaye.Add(s);
                     }
                     else
@@ -108,7 +110,7 @@ namespace ProbPotes.services
                             {
                                 string[] s = new string[2];
                                 s[0] = expense.description;
-                                s[1] = expense.sum.ToString();
+                                s[1] = expense.sum.ToString()+" €";
                                 DepenseBeneficie.Add(s);
                             }
                         }
@@ -135,6 +137,9 @@ namespace ProbPotes.services
 
 
                 //TABLEAU DEPENSE PAYE
+                int cptIsEmpty = 0;
+
+
                 Paragraph titleDepense = new Paragraph("Dépense payé: ", h3);
                 doc.Add(titleDepense);
 
@@ -142,37 +147,30 @@ namespace ProbPotes.services
                 doc.Add(space);
 
 
-                PdfPTable tabPayeur = new PdfPTable(2);
+                PdfPTable tabPayeur = new PdfPTable(3);
                 tabPayeur.WidthPercentage = 75;
 
-                this.addCellToTab("Description", th, blue, tabPayeur);
-                this.addCellToTab("Montant", th, blue, tabPayeur);
+                this.addThToTab("Description", th, blue, tabPayeur,2);
+                this.addThToTab("Montant", th, blue, tabPayeur,1);
 
                 int cptCell = 0;
                 foreach (String[] valeur in DepensePaye)
                 {
                     foreach (string info in valeur)
                     {
-                        PdfPCell cell;
                         if (cptCell % 2 == 0)
                         {
-                            cell = new PdfPCell(new Phrase(info));
-
+                            this.addCellToTab(info, gris, tabPayeur, 2);
                         }
                         else
                         {
-                            cell = new PdfPCell(new Phrase(info + " €"));
-                            cell.HorizontalAlignment = Element.ALIGN_CENTER;
-
+                            this.addCellToTab(info, gris,tabPayeur,1);
                         }
-                        cptCell++;
-                        cell.BackgroundColor = gris;
-                        cell.Padding = 7;
-                        cell.BorderColorBottom = BaseColor.BLACK;
-                        tabPayeur.AddCell(cell);
-
+                        cptIsEmpty++;
                     }
                 }
+                this.tabIsEmpty(cptIsEmpty, tabPayeur, gris);
+                cptIsEmpty = 0;
 
                 doc.Add(tabPayeur);
 
@@ -186,64 +184,100 @@ namespace ProbPotes.services
                 doc.Add(space);
                 doc.Add(space);
 
-                PdfPTable tabBeneficiaire = new PdfPTable(2);
+                PdfPTable tabBeneficiaire = new PdfPTable(3);
                 tabBeneficiaire.WidthPercentage = 75;
 
-                this.addCellToTab("Description", th, blue, tabBeneficiaire);
-                this.addCellToTab("Montant", th, blue, tabBeneficiaire);
-
-
-                foreach (Expense expense in evt.Expenses.Expenses)
-                {
-                    if (expense.creatorCode == part.Code)
-                    {
-                        string[] s = new string[2];
-                        s[0] = expense.description;
-                        s[1] = expense.sum.ToString();
-                        DepensePaye.Add(s);
-                    }
-                    else
-                    {
-                        foreach (int i in expense.recipients)
-                        {
-                            if (i == part.Code)
-                            {
-                                string[] s = new string[2];
-                                s[0] = expense.description;
-                                s[1] = expense.sum.ToString();
-                                DepenseBeneficie.Add(s);
-                            }
-                        }
-                    }
-                }
+                this.addThToTab("Description", th, blue, tabBeneficiaire,2);
+                this.addThToTab("Montant", th, blue, tabBeneficiaire,1);
 
                 cptCell = 0;
                 foreach (String[] valeur in DepenseBeneficie)
                 {
                     foreach (string info in valeur)
                     {
-                        PdfPCell cell;
                         if (cptCell % 2 == 0)
                         {
-                            cell = new PdfPCell(new Phrase(info));
-
+                            this.addCellToTab(info, gris, tabBeneficiaire, 2);
                         }
                         else
                         {
-                            cell = new PdfPCell(new Phrase(info + " €"));
-                            cell.HorizontalAlignment = Element.ALIGN_CENTER;
-
+                            this.addCellToTab(info, gris, tabBeneficiaire, 1);
                         }
-                        cptCell++;
-                        cell.BackgroundColor = gris;
-                        cell.Padding = 7;
-                        cell.BorderColorBottom = BaseColor.BLACK;
-                        tabBeneficiaire.AddCell(cell);
-
+                        cptIsEmpty++;
                     }
                 }
+                this.tabIsEmpty(cptIsEmpty, tabBeneficiaire, gris);
+                cptIsEmpty = 0;
 
                 doc.Add(tabBeneficiaire);
+
+                doc.Add(space);
+                doc.Add(space);
+                doc.Add(space);
+                doc.Add(space);
+                doc.Add(space);
+
+                //TABLEAU GIVETO
+
+                EventManager em = new EventManager();
+                WOWTW wwPart = em.GetWOWTWsPart(evt, part);
+                ParticipantManager pm = new ParticipantManager();
+
+                Paragraph titleGiveTo = new Paragraph(part.Name+" "+part.FirstName+" doit payer à:", h3);
+                doc.Add(titleGiveTo);
+
+                doc.Add(space);
+                doc.Add(space);
+
+                PdfPTable tabGiveTo = new PdfPTable(3);
+                tabGiveTo.WidthPercentage = 75;
+
+                this.addThToTab("Nom", th, blue, tabGiveTo,2);
+                this.addThToTab("Montant", th, blue, tabGiveTo,1);
+
+                foreach (KeyValuePair<int,decimal> val in wwPart.GiveTo)
+                {
+                    Participant p = pm.GetParticipant(val.Key);
+                    this.addCellToTab(p.Name.ToString() + " " + p.FirstName.ToString(), gris, tabGiveTo, 2);
+
+                    this.addCellToTab(val.Value.ToString() + " €", gris, tabGiveTo, 1);
+                    cptIsEmpty++;
+                }
+                this.tabIsEmpty(cptIsEmpty, tabGiveTo, gris);
+                cptIsEmpty = 0;
+
+                doc.Add(tabGiveTo);
+
+                doc.Add(space);
+                doc.Add(space);
+
+                //TABLEAU RECEIVEFROM
+
+                Paragraph titleReceiveFrom = new Paragraph(part.Name + " " + part.FirstName + " doit recevoir de:", h3);
+                doc.Add(titleReceiveFrom);
+
+                doc.Add(space);
+                doc.Add(space);
+
+                PdfPTable tabReceiveFrom = new PdfPTable(3);
+                tabReceiveFrom.WidthPercentage = 75;
+
+                this.addThToTab("Nom", th, blue, tabReceiveFrom,2);
+                this.addThToTab("Montant", th, blue, tabReceiveFrom,1);
+
+                foreach (KeyValuePair<int, decimal> val in wwPart.ReceiveFrom)
+                {
+                    Participant p = pm.GetParticipant(val.Key);
+                    this.addCellToTab(p.Name.ToString() + " " + p.FirstName.ToString(), gris, tabReceiveFrom, 2);
+
+                    this.addCellToTab(val.Value.ToString() + " €", gris, tabReceiveFrom, 1);
+                    cptIsEmpty++;
+                }
+                this.tabIsEmpty(cptIsEmpty, tabReceiveFrom, gris);
+                cptIsEmpty = 0;
+
+                doc.Add(tabReceiveFrom);
+
 
                 doc.Close();
                 Process.Start(@"cmd.exe ", @"/c " + outFile);
@@ -255,14 +289,40 @@ namespace ProbPotes.services
             }
         }
 
-        private void addCellToTab(string str, Font f, BaseColor c, PdfPTable t)
+        private void addThToTab(string str, Font f, BaseColor c, PdfPTable t,int colSpan)
         {
-            PdfPCell cell1 = new PdfPCell(new Phrase(str, f));
-            cell1.BackgroundColor = c;
-            cell1.Padding = 5;
-            cell1.BorderColor = c;
-            cell1.HorizontalAlignment = Element.ALIGN_CENTER;
-            t.AddCell(cell1);
+            PdfPCell cell = new PdfPCell(new Phrase(str, f));
+            cell.BackgroundColor = c;
+            cell.Colspan = colSpan;
+            cell.Padding = 5;
+            cell.BorderColor = c;
+            cell.HorizontalAlignment = Element.ALIGN_CENTER;
+            t.AddCell(cell);
+        }
+
+        private void addCellToTab(string str, BaseColor c, PdfPTable t, int colSpan)
+        {
+            PdfPCell cell = new PdfPCell(new Phrase(str));
+            cell.BackgroundColor = c;
+            cell.Colspan = colSpan;
+            cell.Padding = 7;
+            cell.BorderColorBottom = BaseColor.BLACK;
+            cell.HorizontalAlignment = Element.ALIGN_CENTER;
+            t.AddCell(cell);
+        }
+
+        private void tabIsEmpty(int cpt,PdfPTable t,BaseColor c)
+        {
+            if (cpt == 0)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase("Aucune ligne trouvée :("));
+                cell.BackgroundColor = c;
+                cell.Colspan = 3;
+                cell.Padding = 7;
+                cell.BorderColorBottom = BaseColor.BLACK;
+                cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                t.AddCell(cell);
+            }
         }
     }
 }
